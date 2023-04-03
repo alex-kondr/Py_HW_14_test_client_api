ccb1a4fc5fbc9c7e1c6a96a6397600638716fb9c
alex_kondr <alex_kondr@outlook.com>
2023-04-03 20:11:03 +0300

    save

--- src/routes/static/singup.js
+++ src/routes/static/singup.js
@@ -3,28 +3,28 @@ const form = document.forms[0]
 form.addEventListener('submit', async event => {
     event.preventDefault()
 
-    console.log("form birthday: ", form.birthday.value)
+    // console.log("form birthday: ", form.birthday.value)
 
     
     //     console.log("body: ", body)
     const response = await fetch(
         'https://SilentDismalSweepsoftware.olieksandrkond3.repl.co/api/auth/singup',
-        {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                first_name: form.first_name.value,
-                last_name: form.last_name.value,
-                username: form.username.value,
-                birthday: form.birthday.value,
-                job: form.job.value,
-                email: form.email.value,
-                phone: form.phone.value,
-                password: form.password.value
-            })      
-        }
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    first_name: form.first_name.value ? form.first_name.value:null,
+                    last_name: form.last_name.value ? form.last_name.value:null,
+                    username: form.username.value ? form.username.value:null,
+                    birthday: form.birthday.value ? form.birthday.value:null,
+                    job: form.job.value ? form.job.value:null,
+                    email: form.email.value,
+                    phone: form.phone.value ? form.phone.value:null,
+                    password: form.password.value
+                })      
+            }
     )
 
 
@@ -32,22 +32,36 @@ form.addEventListener('submit', async event => {
     console.log("response status", response.status)
     console.log("result: ", result.detail)
 
-    // if (response.status === 201) {
-    form.hidden = true
-    k = result.detail
-    results = []
-    for (res of k) {
-        results.push(res.msg)
-    }
-    detail.innerHTML = results
-    buttons.hidden = false
+    if (response.status === 201) {
 
-    // console.log("result: ", results)
-//     }
-//   else if (response.status === 401) {
-//     message.innerHTML = result.detail
-//     message.hidden = false
-//   }
+        // const response_avatar = await fetch(
+        //     'https://SilentDismalSweepsoftware.olieksandrkond3.repl.co/api/users/avatar',
+        //         {
+        //             method: 'PATCH',
+        //             headers: {
+        //                 'Content-Type': 'multipart/for-data'
+        //             },
+        //             body: ({
+        //                 file: form.avatar.value
+        //             })      
+        //         }
+        // )
+
+        // console.log("avatar: ", response_avatar)
+
+        form.hidden = true
+        detail.innerHTML = result.detail
+        buttons.hidden = false
+    }
+    else {
+        form.hidden = true
+        buttons.hidden = false
+        msg = ""
+        for (res of result.detail) {
+            msg = msg + res.loc[1] + "-error: " + res.msg + "\n"
+        }
+        detail.innerHTML = msg
+    }
 
 })
 
